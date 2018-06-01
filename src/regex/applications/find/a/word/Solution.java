264034996bee503bb3ab31cdbcb56b9c1d630b86
Alex Anastasopol <aanastasopol@gmail.com>
2018-06-01 09:55:50 +0300

    Finished impl

--- src/regex/applications/find/a/word/Solution.java
+++ src/regex/applications/find/a/word/Solution.java
@@ -9,23 +9,33 @@ import java.util.regex.*;
 public class Solution {
 	private static int getNoOfOccurrences(String word, String[] sentences) {
 		int noOfOccurrences = 0;
-		// TODO: implement
+		Pattern pat = Pattern.compile("\\b" + word + "\\b");
+		for (int i = 0; i < sentences.length; i++) {
+			Matcher mat = pat.matcher(sentences[i]);
+			while (mat.find()) {
+				noOfOccurrences++;
+			}
+		}
+
 		return noOfOccurrences;
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
 		try {
-			int sentencesCount = sc.nextInt();
+			int sentencesCount = Integer.valueOf(sc.nextLine());
 			String[] sentences = new String[sentencesCount];
 			for (int i = 0; i < sentencesCount; i++) {
 				sentences[i] = sc.nextLine();
 			}
 
-			int wordCount = sc.nextInt();
+			int wordCount = Integer.valueOf(sc.nextLine());
 			String[] words = new String[wordCount];
-			for (int i = 0; i < sentencesCount; i++) {
+			for (int i = 0; i < wordCount; i++) {
+				words[i] = sc.nextLine();
+			}
+			
+			for (int i = 0; i < wordCount; i++) {
 				System.out.println(getNoOfOccurrences(words[i], sentences));
 			}
 		} catch (Exception e) {
