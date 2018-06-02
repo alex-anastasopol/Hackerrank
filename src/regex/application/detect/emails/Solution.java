bddb3d1ec190e741cc667dacaf9f078908eb050a
Alex Anastasopol <aanastasopol@gmail.com>
2018-06-02 09:45:03 +0300

    Adding package for 'detect emails' challenge

--- src/regex/application/detect/emails/Solution.java
+++ src/regex/application/detect/emails/Solution.java
@@ -1,9 +1,45 @@
 import java.io.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
+	private static int getNoOfOccurrences(String word, String[] sentences) {
+		int noOfOccurrences = 0;
+		Pattern pat = Pattern.compile("\\b" + word + "\\b");
+		for (int i = 0; i < sentences.length; i++) {
+			Matcher mat = pat.matcher(sentences[i]);
+			while (mat.find()) {
+				noOfOccurrences++;
+			}
+		}
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-    }
+		return noOfOccurrences;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try {
+			int sentencesCount = Integer.valueOf(sc.nextLine());
+			String[] sentences = new String[sentencesCount];
+			for (int i = 0; i < sentencesCount; i++) {
+				sentences[i] = sc.nextLine();
+			}
+
+			int wordCount = Integer.valueOf(sc.nextLine());
+			String[] words = new String[wordCount];
+			for (int i = 0; i < wordCount; i++) {
+				words[i] = sc.nextLine();
+			}
+			
+			for (int i = 0; i < wordCount; i++) {
+				System.out.println(getNoOfOccurrences(words[i], sentences));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sc.close();
+		}
+	}
 }
